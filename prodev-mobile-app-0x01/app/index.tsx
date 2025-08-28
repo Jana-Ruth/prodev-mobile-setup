4480adfc15e0f92201eb0015785a23d6f628452a
Jana-Ruth <ruthewenode73@gmail.com>
2025-08-28 14:47:05 +0100

    please add e

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -17,14 +17,15 @@ export default function HomeScreen() {
         />
       }>
       <View style={styles.container}>
-        <Text>Entry Screen - Awsome</Text>
+        <Text>Entry Screen - Awesome</Text>
         <HelloWave />
       </View>
-      <View>
+    <View>
   <Text style={styles.largeText}>Typescript is great if you practice more</Text>
   <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
   <Text style={styles.smallText}>ALX is awesome</Text>
 </View>
+
 
       
     </ParallaxScrollView>
