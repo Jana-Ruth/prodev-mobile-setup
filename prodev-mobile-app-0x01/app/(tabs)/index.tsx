80f571040f0fefbd2202f4244fdcfd36ac245972
Jana-Ruth <ruthewenode73@gmail.com>
2025-08-28 15:13:42 +0100

    three text

--- prodev-mobile-app-0x01/app/(tabs)/index.tsx
+++ prodev-mobile-app-0x01/app/(tabs)/index.tsx
@@ -17,10 +17,10 @@ export default function HomeScreen() {
         />
       }>
       <View style={styles.container}>
-        <Text>Entry Screen - Awesome</Text>
+        <Text>"Entry Screen - Awesome"</Text>
         <HelloWave />
       </View>
-    <View>
+  <View>
   <Text style={styles.largeText}>Typescript is great if you practice more</Text>
   <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
   <Text style={styles.smallText}>ALX is awesome</Text>
